8323d546171027e77755f3b9c913c1e838212333
nagappa <naguparu1419@gmail.com>
2022-09-15 14:00:43 +0530

    first commit

---
+++ src/main/java/com/example/demojenkins/DemojenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojenkinsApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hey bro");
+	}
+
+}
